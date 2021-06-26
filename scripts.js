4046ade4ff492b34eb1f0fcce29944c63862689e
Xavier Hancock <Xavierhancocktech@gmail.com>
2021-06-26 22:42:03 +1000

    Basic formatting

--- scripts.js
+++ scripts.js
@@ -20,6 +20,14 @@ function openPage(pageName, elmnt, color) {
   
 }
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+} 
 
 
